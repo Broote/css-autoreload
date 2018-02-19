b6270a2331792301f70fe1bff6c47645508b60ea
Thomas Günther <mail@medienbaecker.com>
2018-02-19 16:42:00 +0100

    Allow multiple stylesheets

--- css-autoreload.js
+++ css-autoreload.js
@@ -4,9 +4,12 @@ function fetchModified(url) {
     req.send(null);
     return req.getResponseHeader('Last-Modified');
 }
-var styleSheet = document.querySelector('link[data-reload="true"]');
-var styleSheetUrl = styleSheet.href.split('?')[0];
+var styleSheets = document.querySelectorAll('link[data-reload="true"]');
+console.log(styleSheets);
 setInterval(function() {
-    var styleSheetModified = new Date(fetchModified(styleSheetUrl)).getTime();
-    styleSheet.setAttribute('href', styleSheetUrl + "?m=" + styleSheetModified);
+    for (i = 0; i < styleSheets.length; ++i) {
+        var styleSheetUrl = styleSheets[i].href.split('?')[0];
+        var styleSheetModified = new Date(fetchModified(styleSheetUrl)).getTime();
+        styleSheets[i].setAttribute('href', styleSheetUrl + "?m=" + styleSheetModified);
+    }
 }, 500);
