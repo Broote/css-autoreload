d98c0ddef6fa3cabdf27109a45b9338cbc7bfd2f
Thomas Günther <mail@medienbaecker.com>
2018-02-27 10:14:09 +0100

    Better error handling

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This script regularly checks if any of your CSS files were modified and reloads 
 Use it as a bookmarklet:
 
 ```
-javascript:void%20function(){function%20e(e){var%20t=new%20XMLHttpRequest;return%20t.open(%22HEAD%22,e+%22%3Fd=%22+Date.now(),!1),t.send(null),t.getResponseHeader(%22Last-Modified%22)}var%20t=document.querySelectorAll('link[href*=%22.css%22]');setInterval(function(){for(i=0;i%3Ct.length;++i){var%20n=t[i].href.split(%22%3F%22)[0],r=new%20Date(e(n)).getTime();t[i].href.split(%22%3Fm=%22)[1]!=r%26%26t[i].setAttribute(%22href%22,n+%22%3Fm=%22+r)}},1e3)}();
+javascript:void%20function(){var%20e=document.querySelectorAll('link[href*=%22.css%22]');setInterval(function(){for(i=0;i%3Ce.length;++i){var%20t=e[i].href.split(%22%3F%22)[0],n=new%20XMLHttpRequest;if(n.open(%22HEAD%22,t+%22%3Fd=%22+Date.now(),!1),n.send(null),200==n.status){var%20o=new%20Date(n.getResponseHeader(%22Last-Modified%22)).getTime();e[i].href.split(%22%3Fm=%22)[1]!=o%26%26(console.log(%22Reloaded%20%22+t),e[i].setAttribute(%22href%22,t+%22%3Fm=%22+o))}}},1e3)}();
 ```
 
 ![Preview](https://user-images.githubusercontent.com/7975568/36636673-481d701a-19cb-11e8-99c9-681679c30054.gif?raw=true)
--- css-autoreload.js
+++ css-autoreload.js
@@ -1,16 +1,16 @@
-function fetchModified(url) {
-    var req = new XMLHttpRequest();
-    req.open("HEAD", url + "?d=" + Date.now(), false);
-    req.send(null);
-    return req.getResponseHeader('Last-Modified');
-}
 var styleSheets = document.querySelectorAll('link[href*=".css"]');
 setInterval(function() {
     for (i = 0; i < styleSheets.length; ++i) {
-        var styleSheetUrl = styleSheets[i].href.split('?')[0];
-        var styleSheetModified = new Date(fetchModified(styleSheetUrl)).getTime();
-        if(styleSheets[i].href.split('?m=')[1] != styleSheetModified) {
-            styleSheets[i].setAttribute('href', styleSheetUrl + "?m=" + styleSheetModified);
+        var styleSheetUrl = styleSheets[i].href.split('?')[0];                
+        var req = new XMLHttpRequest();
+        req.open("HEAD", styleSheetUrl + "?d=" + Date.now(), false);
+        req.send(null);
+        if(req.status == 200) {
+            var styleSheetModified = new Date(req.getResponseHeader('Last-Modified')).getTime();
+            if(styleSheets[i].href.split('?m=')[1] != styleSheetModified) {
+                console.log("Reloaded " + styleSheetUrl);
+                styleSheets[i].setAttribute('href', styleSheetUrl + "?m=" + styleSheetModified);
+            }
         }
     }
 }, 1000);
